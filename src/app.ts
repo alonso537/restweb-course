7bee3920364a8681cc6a1bc22f45a8c7fd6b30fc
alonso537 <adrianaalonso002mail.com>
2023-11-04 17:34:10 -0600

    diferentes respuestas

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
+import fs from 'fs';
 import http from 'http';
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
 
 
-    res.write('Hola Mundo')
-
-    res.end()
+    // res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('<h1>Hola Mundo!</h1>')
+    // res.end();
     
+    // const data = {
+    //     name: 'Alejandro',
+    //     age: 28,
+    //     city: 'new York'
+    // }
+
+    // res.writeHead(200, {'Content-Type': 'application/json'})
+    // res.end(JSON.stringify(data));
+
+    if(req.url === '/') {
+        const htmlFile = fs.readFileSync('./public/index.html', 'utf8');
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(htmlFile);
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end()
+    }
+
 })
 
 server.listen(8080, () => {

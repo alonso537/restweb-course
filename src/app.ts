a184073f66d2ca5866d9e9107ff488342071b816
alonso537 <adrianaalonso002mail.com>
2023-11-04 17:25:46 -0600

    first web server

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import http from 'http';
+
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+
+
+    res.write('Hola Mundo')
+
+    res.end()
+    
+})
+
+server.listen(8080, () => {
+    console.log('Server running 8080');
+    
+})

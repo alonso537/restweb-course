88f074e7660cab6deec30fe03908c4ecede2472d
alonso537 <adrianaalonso002mail.com>
2023-11-04 18:03:58 -0600

    express

--- src/app.ts
+++ src/app.ts
@@ -1,42 +1,15 @@
-import fs from 'fs';
-import http from 'http';
-
-const server = http.createServer((req, res) => {
-    console.log(req.url);
+import { Server } from "./presentation/server"
 
 
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write('<h1>Hola Mundo!</h1>')
-    // res.end();
+(() => {
+    main()
+})()
+
+function main() {
+    // console.log('main');
+
+    const server = new Server()
+
+    server.start()
     
-    // const data = {
-    //     name: 'Alejandro',
-    //     age: 28,
-    //     city: 'new York'
-    // }
-
-    // res.writeHead(200, {'Content-Type': 'application/json'})
-    // res.end(JSON.stringify(data));
-
-    if(req.url === '/') {
-        const htmlFile = fs.readFileSync('./public/index.html', 'utf8');
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(htmlFile);
-        return;
-    } 
-
-    if(req.url?.endsWith('.js')) {
-        res.writeHead(200, {'Content-Type': 'application/javascript'})
-    } else if(req.url?.endsWith('.css')) {
-        res.writeHead(200, {'Content-Type': 'text/css'})
-    }
-
-    const responseContent = fs.readFileSync(`./public${req.url}`, 'utf-8')
-    res.end(responseContent)
-
-})
-
-server.listen(8080, () => {
-    console.log('Server running 8080');
-    
-})
+}

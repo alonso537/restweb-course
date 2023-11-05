694233c5d4d0575508d9e8cff6daba88b57e27d5
alonso537 <adrianaalonso002mail.com>
2023-11-05 14:59:05 -0600

    router and controller

---
+++ src/presentation/routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+
+
+export class AppRoutes {
+    static get routes(): Router {
+        const router = Router();
+
+        router.get('/api/todos', )
+
+
+        return router;
+    }
+}

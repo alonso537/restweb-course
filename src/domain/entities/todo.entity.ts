127c4498a0878b5b56d423c97138d1609f7daf56
alonso537 <adrianaalonso002mail.com>
2023-11-11 20:55:50 -0600

    datasource implements

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -1,15 +1,27 @@
-
-
 export class TodoEntity {
-    constructor(
-        public id: number,
-        public text: string,
-        public completedAt?: Date|null
-    ) { }
+  constructor(
+    public id: number,
+    public text: string,
+    public completedAt?: Date | null
+  ) {}
 
-    get isCompleted() {
-        return !!this.completedAt;
+  get isCompleted() {
+    return !!this.completedAt;
+  }
+
+  public static fromObject(object: { [key: string]: any }): TodoEntity {
+    const { id, text, completedAt } = object;
+    if (!id) throw new Error(`id property is required`);
+    if (!text) throw new Error(`text property is required`);
+
+    let newCompletedAt;
+    if (completedAt) {
+      newCompletedAt = new Date(completedAt);
+      if (isNaN(newCompletedAt.getTime())) {
+        throw `completedAt property is not a valid date`;
+      }
     }
 
-    
+    return new TodoEntity(id, text, completedAt);
+  }
 }
